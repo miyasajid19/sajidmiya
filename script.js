f85735b2807a2032ddc4615a383e6d2aff82ae15
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-11-08 22:39:13 +0530

    update

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const laserPointer = document.createElement('div');
     laserPointer.classList.add('laser-pointer');
     document.body.appendChild(laserPointer);
 
-    document.body.addEventListener('mousemove', function(e) {
+    document.body.addEventListener('mousemove', function (e) {
         laserPointer.style.display = 'block';
         laserPointer.style.left = e.clientX + 'px';
         laserPointer.style.top = e.clientY + 'px';
     });
 
-    document.body.addEventListener('mouseleave', function() {
+    document.body.addEventListener('mouseleave', function () {
         laserPointer.style.display = 'none';
     });
 
-    document.body.addEventListener('mouseenter', function() {
+    document.body.addEventListener('mouseenter', function () {
         laserPointer.style.display = 'block';
     });
 });
 let clickCount = 0;
 
-        document.addEventListener('click', () => {
-            clickCount++;
-            if (clickCount === 7) {
-                document.getElementById('secret-message').style.display = 'block';
-                document.getElementById('initial-message').style.display = 'none';
+document.addEventListener('click', () => {
+    clickCount++;
+    if (clickCount === 407) {
+        document.getElementById('secret-message').style.display = 'block';
+        document.getElementById('initial-message').style.display = 'none';
 
-            }
-        });
+    }
+});
